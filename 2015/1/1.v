840d81b2937aa7c174d14d140327c945fdedad0e
wessel <wessel@factorit.nl>
2023-11-09 09:16:56 +0100

    Completed the very first advent of code (2015) in v

---
+++ 2015/1/1.v
@@ -0,0 +1,29 @@
+import os
+
+fn main() {
+	input_file := os.read_file('input') or {
+		println('Error: no input file found')
+		return
+	}
+
+	mut floor := 0;
+	mut reached_basement := false;
+
+	for index, elem in input_file.runes() {
+		if elem == `(` {
+			floor = floor + 1
+		} else if elem == `)` {
+			floor = floor - 1
+		}
+		
+		// Part two
+		if !reached_basement && floor < 0 {
+			reached_basement = true
+
+			println('Santa first entered the basement at position: ${index + 1}')
+		}
+	}
+
+	// Part one
+	println('Santa arrives at floor: ${floor}')
+}
