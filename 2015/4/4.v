1b4636cdd126498a8e3eb42eb10ab778c9ffac57
twiglight <twiglight@localhost.localdomain>
2023-11-09 20:35:20 +0100

    Finished day 4, albeit with brute force

---
+++ 2015/4/4.v
@@ -0,0 +1,21 @@
+import os
+import crypto.md5
+
+fn main() {
+	input_file := os.read_file('input') or {
+		panic('Can\'t read input file!')
+	}.trim_space()
+
+	mut secret := 0
+
+	for {
+		hash := md5.hexhash('${input_file}${secret}')
+		
+		if hash.starts_with('000000') {
+			println(secret)
+			break
+		} else {
+			secret = secret + 1
+		}
+	}
+}
