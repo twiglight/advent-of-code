2ef192ecb7c9e22fa26c49dfcdc0f5596c18e66e
twiglight <twiglight@localhost.localdomain>
2023-12-02 11:34:10 +0100

    Removed the 'or panics' from 2023 day 2

--- 2023/2/2_2.v
+++ 2023/2/2_2.v
@@ -7,13 +7,13 @@ fn main() {
 
 	possible_games := input.map(fn (game string) int {
 		colors := ['red', 'green', 'blue'].map(fn [game] (color string) int {
-			mut r := regex.regex_opt('\\d+ ${color}') or { panic('Could not create regex!') }
-			return arrays.max[int](r.find_all_str(game.split(':').last()).map(it.split(' ').first().int())) or { panic('WOW') }
+			mut r, _, _ := regex.regex_base('\\d+ ${color}')
+			return arrays.max[int](r.find_all_str(game.split(':').last()).map(it.split(' ').first().int())) or { -1 }
 		})
 
 		return arrays.reduce(colors, fn (acc int, elem int) int {
 			return acc * elem
-		}) or { panic('help!') }
+		}) or { -1 }
 	})
 	
 	println('The sum of all possible games ids is ${arrays.sum(possible_games)!}')
