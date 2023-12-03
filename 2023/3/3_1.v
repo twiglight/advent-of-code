edfd1f135f0a537811c9d59aba0559655a1d4d8a
twiglight <twiglight@localhost.localdomain>
2023-12-03 17:19:15 +0100

    Improved part 1 a bit

--- 2023/3/3_1.v
+++ 2023/3/3_1.v
@@ -2,36 +2,36 @@ import os
 import regex
 import arrays
 
-fn is_part_number(lines []string, index int, start int, end int) bool {
+fn is_part_number(lines []string, index int, start int, end int) int {
 	for y := index-1; y <= index+1; y+=1 {
 		if y < 0 { continue }
-		if y >= lines.len { return false }
+		if y >= lines.len { return 0 }
 
 		for x := start-1; x < end+1; x+=1 {
 			if x < 0 { continue }
 			if x >= lines[index].len { continue }
 
-			if lines[y][x] != `.` && !lines[y][x].is_digit() { return true }
+			if lines[y][x] != `.` && !lines[y][x].is_digit() { return lines[index][start..end].int() }
 		}
 	} 
 
-	return false
+	return 0
 }
 
 fn main() {
 	input := os.read_lines('input') or { panic('Could not open input file!') }
 
 	mut r, _, _ := regex.regex_base('\\d+')
-	parsed_input := arrays.map_indexed(input, fn [mut r, input] (index int, line string) string {
-		return r.replace_by_fn(line, fn [input, index] (re regex.RE, in_txt string, start int, end int) string {
+	parsed_input := arrays.flat_map_indexed[string, int](input, fn [mut r, input] (index int, line string) []int {
+		matches := r.find_all(line)
+		mut numbers := []int{}
 
-			if is_part_number(input, index, start, end) {
-				return in_txt[start..end]
-			}
+		for x := 0; x < matches.len; x+=2 {
+			numbers << is_part_number(input, index, matches[x], matches[x+1])
+		}
 
-			return in_txt[start..end].runes().map(fn (c rune) rune {return `.`}).string()
-		})
-	}).join('')
-	
-	println('Engine code: ${arrays.fold(r.find_all_str(parsed_input), 0, fn (acc int, elem string) int {return acc + elem.int()})}')
+		return numbers
+	})
+
+	println('Engine code: ${arrays.sum(parsed_input) or {0}}')
 }
