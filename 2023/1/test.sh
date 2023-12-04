8b8428557e5af9d270ab2625c84dbb2fa48a896d
twiglight <twiglight@localhost.localdomain>
2023-12-04 23:42:37 +0100

    Tried my had at Arturo

--- 2023/1/test.sh
+++ 2023/1/test.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bats
 
 @test "Test for Part 1 (vlang)" {
+  result="$(v run 1_1.ar)"
+  [[ "$result" =~ "55712" ]]
+}
+
+@test "Test for Part 1 (vlang)" {
   result="$(v run 1_1.v)"
   [[ "$result" =~ "55712" ]]
 }
